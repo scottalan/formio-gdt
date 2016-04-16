23439c7b59c20cd08304655a77523d234ac11ffd
Scott Alan Henry <scottalanhenry@gmail.com>
2016-04-16 02:12:42 -0500

    The new template that uses an iframe that uses the 'view' dist from formio-viewer

--- src/modules/formio/plugins/content_types/theme/formio-form.tpl.php
+++ src/modules/formio/plugins/content_types/theme/formio-form.tpl.php
@@ -1,6 +1,18 @@
-<?php
-$stop = '';
-?>
-
-<!--  <formio src="'https://ukpryfgoedtaslz.form.io/registration'"></formio>-->
-<formio src="'https://ukpryfgoedtaslz.form.io/<?php print $element['#path']; ?>'"></formio>
+<script type="text/javascript">(function a(u) {
+    if (typeof jQuery === u) {
+      return setTimeout(a, 100);
+    }
+    document.write('<script src="https://npmcdn.com/seamless@latest"><\/script>');
+    (function b($) {
+      if (typeof $.fn.seamless === u) {
+        return setTimeout(b, 100);
+      }
+      $(function () {
+        $('#formio-form').seamless({fallback: false}).receive(function (d, e) {
+        });
+      });
+    })(jQuery);
+  })();</script>
+<iframe id="formio-form" style="width:100%;border:none;" height="600px"
+  src="<?php print $module_path; ?>/_view/index.html#/<?php print $project_hash; ?>/form/<?php print $_id; ?>?iframe=1<?php print $bootswatch_theme; ?>&header=0">
+</iframe>
