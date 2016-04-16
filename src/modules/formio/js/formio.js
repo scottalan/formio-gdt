b9f716dffde6f8be1866fb95ff7170c53ea43521
Scott Alan Henry <scottalanhenry@gmail.com>
2016-04-16 01:49:52 -0500

    Not deleting this file just yet.
    * Removing js dealing with angular.
    * We are using the iframe along with the formio-viewer dist.

--- src/modules/formio/js/formio.js
+++ src/modules/formio/js/formio.js
@@ -1,27 +1,15 @@
-// Using the closure to map jQuery to $.
+// Using a closure to map jQuery to $.
 (function ($, Drupal) {
 
-  $(function() {
-    angular.module('formApp', ['formio']);
-    angular.bootstrap(document, ['formApp']);
-  });
-
-  if (typeof Drupal.ajax !== 'undefined') {
-    Drupal.ajax.prototype.commands.formioRender = function (ajax, response) {
-      // The parent of the new element
-      var $target = $(response.target);
-
-      // The new element to be added
-      //var $formio = $('<formio src="\'https://ukpryfgoedtaslz.form.io/' + form + '\'"></formio>');
-      var $formio = $('<formio src="\'' + response.api + '/' + response.form + '\'"></formio>');
-
-      angular.element($target).injector().invoke(function ($compile) {
-        var $scope = angular.element($target).scope();
-        $target.html($compile($formio)($scope));
-        // Finally, refresh the watch expressions in the new element
-        $scope.$apply();
-      });
-    };
-  }
+  Drupal.behaviors.formioRender = {
+    attach: function (context, settings) {
+      if (settings.formioRender) {
+        $(document).ready(function () {
+          // Do something when form renders.
+        });
+      }
+    },
+    detach: function (context) {}
+  };
 
 }(jQuery, Drupal));
