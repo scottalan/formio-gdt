1408c2514e6a59f2b2e5c18e5fcd14bce7b3fcb1
Scott Alan Henry <scottalanhenry@gmail.com>
2016-04-22 17:28:31 -0400

    Updating the script that is used in the template per @travist instructions.

--- src/modules/formio/plugins/content_types/theme/formio-form.tpl.php
+++ src/modules/formio/plugins/content_types/theme/formio-form.tpl.php
@@ -1,18 +1,15 @@
-<script type="text/javascript">(function a(u) {
-    if (typeof jQuery === u) {
-      return setTimeout(a, 100);
-    }
-    document.write('<script src="https://npmcdn.com/seamless@latest"><\/script>');
-    (function b($) {
-      if (typeof $.fn.seamless === u) {
-        return setTimeout(b, 100);
-      }
-      $(function () {
-        $('#formio-form').seamless({fallback: false}).receive(function (d, e) {
-        });
-      });
-    })(jQuery);
-  })();</script>
+<script type="text/javascript">
+(function b($, u) {
+  if (typeof $.fn.seamless === u) {
+    return setTimeout(b, 100);
+  }
+  $(function () {
+    $('#formio-form').seamless({fallback: false}).receive(function (d, e) {
+    });
+  });
+})(jQuery);
+</script>
+
 <iframe id="formio-form" style="width:100%;border:none;" height="600px"
   src="<?php print $module_path; ?>/_view/index.html#/<?php print $project_hash; ?>/form/<?php print $_id; ?>?iframe=1<?php print $bootswatch_theme; ?>&header=0">
 </iframe>
