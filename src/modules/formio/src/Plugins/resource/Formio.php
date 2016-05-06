ed69ceeb44e9cf1c1545b2631a6776c148ccd0f1
Randall Knutson <randall@form.io>
2016-05-05 20:06:53 -0500

    Add entity and start adding fields for entities.

---
+++ src/modules/formio/src/Plugins/resource/Formio.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ *
+ * Contails \Drupal\formio\Plugin\resource\Formio
+ * Created by PhpStorm.
+ * User: randallknutson
+ * Date: 5/4/16
+ * Time: 11:15 AM
+ */
+
+namespace Drupal\formio\Plugin\resource;
+
+
+use Drupal\restful\Plugin\resource\ResourceEntity;
+
+class Formio extends ResourceEntity
+{
+
+}
